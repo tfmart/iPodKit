82bb5d080443633dd61b4d0a9196874bc1f9dc83
Tomás Martins <tomas.marti@me.com>
2026-01-25 01:51:03 -0300

    Add extended track metadata fields to ITDBTrack

--- Sources/iPodKit/Databases/Standard/ITDBTrack.swift
+++ Sources/iPodKit/Databases/Standard/ITDBTrack.swift
@@ -78,6 +78,7 @@ public struct ITDBTrack: IPKParseable, Sendable {
     public let numberOfStrings: UInt32
     public let uniqueId: UInt32
     public let visible: UInt32
+    public let compilationFlag: UInt8
     public let rating: UInt8
     public let lastModified: UInt32
     public let size: UInt32
@@ -87,9 +88,18 @@ public struct ITDBTrack: IPKParseable, Sendable {
     public let year: UInt32
     public let bitrate: UInt32
     public let sampleRate: UInt32
+    public let volumeAdjustment: Int32
+    public let startTime: UInt32
+    public let stopTime: UInt32
+    public let soundCheck: UInt32
     public let playCount: UInt32
     public let lastPlayed: UInt32
+    public let discNumber: UInt32
+    public let totalDiscs: UInt32
+    public let dateAdded: UInt32
     public let dbid: UInt64
+    public let bpm: UInt16
+    public let mediaType: UInt32
 
     // Parsed string metadata
     public let title: String?
@@ -103,7 +113,7 @@ public struct ITDBTrack: IPKParseable, Sendable {
     
     public init(from data: Data) throws {
         try Self.validateMagicNumber(from: data, expectedId: "mhit")
-        
+
         // Parse binary fields
         self.headerLength = try Self.HeaderLength().readUInt32(from: data)
         self.totalLength = try Self.TotalLength().readUInt32(from: data)
@@ -111,6 +121,7 @@ public struct ITDBTrack: IPKParseable, Sendable {
         self.uniqueId = try Self.Identifier().readUInt32(from: data)
         self.visible = try Self.Visible().readUInt32(from: data)
         _ = try Self.FileType().readUInt32(from: data)
+        self.compilationFlag = try Self.CompilationFlag().readUInt8(from: data)
         self.rating = try Self.Rating().readUInt8(from: data)
         self.lastModified = try Self.LastModified().readUInt32(from: data)
         self.size = try Self.Size().readUInt32(from: data)
@@ -120,9 +131,18 @@ public struct ITDBTrack: IPKParseable, Sendable {
         self.year = try Self.Year().readUInt32(from: data)
         self.bitrate = try Self.Bitrate().readUInt32(from: data)
         self.sampleRate = try Self.SampleRate().readUInt32(from: data)
+        self.volumeAdjustment = try Self.VolumeAdjustment().readInt32(from: data)
+        self.startTime = try Self.StartTime().readUInt32(from: data)
+        self.stopTime = try Self.StopTime().readUInt32(from: data)
+        self.soundCheck = try Self.SoundCheck().readUInt32(from: data)
         self.playCount = try Self.PlayCount().readUInt32(from: data)
         self.lastPlayed = try Self.LastPlayed().readUInt32(from: data)
+        self.discNumber = try Self.DiscNumber().readUInt32(from: data)
+        self.totalDiscs = try Self.TotalDiscs().readUInt32(from: data)
+        self.dateAdded = try Self.DateAdded().readUInt32(from: data)
         self.dbid = try Self.DBID().readUInt64(from: data)
+        self.bpm = try Self.BPM().readUInt16(from: data)
+        self.mediaType = try Self.MediaType().readUInt32(from: data)
 
         // Parse string metadata objects
         var stringMetadata: [ITDBDataObject.TypeIdentifier: String] = [:]
@@ -225,6 +245,34 @@ public extension ITDBTrack {
         let unixTimestamp = TimeInterval(lastModified) - macEpochOffset
         return Date(timeIntervalSince1970: unixTimestamp)
     }
+
+    /// Date when track was added to the library
+    var dateAddedDate: Date? {
+        guard dateAdded > 0 else { return nil }
+        let macEpochOffset: TimeInterval = 2082844800
+        let unixTimestamp = TimeInterval(dateAdded) - macEpochOffset
+        return Date(timeIntervalSince1970: unixTimestamp)
+    }
+
+    /// Whether this track is part of a compilation album
+    var isCompilation: Bool {
+        compilationFlag == 1
+    }
+
+    /// Start time in seconds (custom playback start point)
+    var startTimeInSeconds: TimeInterval {
+        TimeInterval(startTime) / 1000.0
+    }
+
+    /// Stop time in seconds (custom playback end point, 0 means play to end)
+    var stopTimeInSeconds: TimeInterval {
+        TimeInterval(stopTime) / 1000.0
+    }
+
+    /// Volume adjustment in decibels (-100% to +100% maps to -255 to +255)
+    var volumeAdjustmentPercent: Int {
+        Int(volumeAdjustment) * 100 / 255
+    }
 }
 
 extension ITDBTrack {
@@ -232,74 +280,99 @@ extension ITDBTrack {
         var offset: Int { 4 }
         var length: Int { 4 }
     }
-    
+
     struct TotalLength: IPKField {
         var offset: Int { 8 }
         var length: Int { 4 }
     }
-    
+
     struct Strings: IPKField {
         var offset: Int { 12 }
         var length: Int { 4 }
     }
-    
+
     struct Identifier: IPKField {
         var offset: Int { 16 }
         var length: Int { 4 }
     }
-    
+
     struct Visible: IPKField {
         var offset: Int { 20 }
         var length: Int { 4 }
     }
-    
+
     struct FileType: IPKField {
         var offset: Int { 24 }
         var length: Int { 4 }
     }
-    
+
+    struct CompilationFlag: IPKField {
+        var offset: Int { 30 }
+        var length: Int { 1 }
+    }
+
     struct Rating: IPKField {
         var offset: Int { 31 }
         var length: Int { 1 }
     }
-    
+
     struct LastModified: IPKField {
         var offset: Int { 32 }
         var length: Int { 4 }
     }
-    
+
     struct Size: IPKField {
         var offset: Int { 36 }
         var length: Int { 4 }
     }
-    
+
     struct Length: IPKField {
         var offset: Int { 40 }
         var length: Int { 4 }
     }
-    
+
     struct TrackNumber: IPKField {
         var offset: Int { 44 }
         var length: Int { 4 }
     }
-    
+
     struct TotalTracks: IPKField {
         var offset: Int { 48 }
         var length: Int { 4 }
     }
-    
+
     struct Year: IPKField {
         var offset: Int { 52 }
         var length: Int { 4 }
     }
-    
+
     struct Bitrate: IPKField {
         var offset: Int { 56 }
         var length: Int { 4 }
     }
-    
+
     struct SampleRate: IPKField {
         var offset: Int { 60 }
+        var length: Int { 4 }
+    }
+
+    struct VolumeAdjustment: IPKField {
+        var offset: Int { 64 }
+        var length: Int { 4 }
+    }
+
+    struct StartTime: IPKField {
+        var offset: Int { 68 }
+        var length: Int { 4 }
+    }
+
+    struct StopTime: IPKField {
+        var offset: Int { 72 }
+        var length: Int { 4 }
+    }
+
+    struct SoundCheck: IPKField {
+        var offset: Int { 76 }
         var length: Int { 4 }
     }
 
@@ -313,8 +386,33 @@ extension ITDBTrack {
         var length: Int { 4 }
     }
 
+    struct DiscNumber: IPKField {
+        var offset: Int { 92 }
+        var length: Int { 4 }
+    }
+
+    struct TotalDiscs: IPKField {
+        var offset: Int { 96 }
+        var length: Int { 4 }
+    }
+
+    struct DateAdded: IPKField {
+        var offset: Int { 104 }
+        var length: Int { 4 }
+    }
+
     struct DBID: IPKField {
         var offset: Int { 112 }
         var length: Int { 8 }
+    }
+
+    struct BPM: IPKField {
+        var offset: Int { 122 }
+        var length: Int { 2 }
+    }
+
+    struct MediaType: IPKField {
+        var offset: Int { 208 }
+        var length: Int { 4 }
     }
 }
