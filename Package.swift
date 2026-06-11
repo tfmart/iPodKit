d4b231deb6702ecd683375c0735d0e8b6e3491d1
Tomás Martins <tomas.marti@me.com>
2026-06-11 20:46:13 -0300

    Replace analyzer tool with ipodkit CLI targets

--- Package.swift
+++ Package.swift
@@ -13,12 +13,13 @@ let package = Package(
             name: "iPodKit",
             targets: ["iPodKit"]),
         .executable(
-            name: "analyze-itunes-db",
-            targets: ["iTunesDBAnalyzer"]),
+            name: "ipodkit",
+            targets: ["iPodKitCLI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.0"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
         .target(
@@ -27,12 +28,28 @@ let package = Package(
                 .product(name: "SQLite", package: "SQLite.swift")
             ]
         ),
+        .target(
+            name: "iPodKitCLICore",
+            dependencies: ["iPodKit"]
+        ),
         .executableTarget(
-            name: "iTunesDBAnalyzer",
-            dependencies: ["iPodKit"]),
+            name: "iPodKitCLI",
+            dependencies: [
+                "iPodKit",
+                "iPodKitCLICore",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
         .testTarget(
             name: "iPodKitTests",
             dependencies: ["iPodKit"],
+            resources: [
+                .copy("Resources")
+            ]
+        ),
+        .testTarget(
+            name: "iPodKitCLITests",
+            dependencies: ["iPodKitCLICore", "iPodKit"],
             resources: [
                 .copy("Resources")
             ]
